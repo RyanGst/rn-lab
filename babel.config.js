6e51b4bb942926ceaf27f2a26b59100fbbf7fac9
ryangst <ryangst.hire@gmail.com>
2024-08-27 20:29:02 -0300

    change definition of babel config

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
-module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            // should be the last plugin
-            'react-native-reanimated/plugin'
-        ]
-    };
+module.exports = (api) => {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			// should be the last plugin
+			"react-native-reanimated/plugin",
+		],
+	};
 };
