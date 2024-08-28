02e7b43dd3bc1f1eee53dec9604305587cbf3622
ryangst <ryangst.hire@gmail.com>
2024-08-28 20:14:00 -0300

    chore: Add @react-native-async-storage/async-storage and @supabase/supabase-js dependencies

---
+++ src/utils/supabase.ts
@@ -0,0 +1,16 @@
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+	process.env.EXPO_PUBLIC_SUPABASE_URL || "",
+	process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
+	{
+		auth: {
+			storage: AsyncStorage,
+			autoRefreshToken: true,
+			persistSession: true,
+			detectSessionInUrl: false,
+		},
+	},
+);
