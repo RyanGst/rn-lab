8a7ef4d940dafb7b9575140cebcf0be2de2f441b
ryangst <ryangst.hire@gmail.com>
2024-08-28 20:34:44 -0300

    create login page

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { FlatList, Text, View } from "react-native";
-import { supabase } from "./utils/supabase";
+import React from "react";
+import { View } from "react-native";
+import Login from "./screens/auth/Login";
 
 export default function App() {
-	const [todos, setTodos] = useState([]);
-
-	useEffect(() => {
-		const getTodos = async () => {
-			try {
-				const { data: todos, error } = await supabase.from("todos").select();
-
-				if (error) {
-					console.error("Error fetching todos:", error.message);
-					return;
-				}
-
-				if (todos && todos.length > 0) {
-					setTodos(todos);
-				}
-			} catch (error) {
-				console.error("Error fetching todos:", error.message);
-			}
-		};
-
-		getTodos();
-	}, []);
-
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>Todo List</Text>
-			<FlatList
-				data={todos}
-				keyExtractor={(item) => item.id.toString()}
-				renderItem={({ item }) => <Text key={item.id}>{item.title}</Text>}
-			/>
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Login />
 		</View>
 	);
 }
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,6 @@
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-
 export const supabase = createClient(
 	process.env.EXPO_PUBLIC_SUPABASE_URL || "",
 	process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
