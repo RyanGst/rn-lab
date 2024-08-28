6f6d5d7067182be63cc68d3a0d875fc5fc6ce28e
ryangst <ryangst.hire@gmail.com>
2024-08-28 20:17:36 -0300

    chore: Update App component to fetch and display todos from Supabase

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { FlatList, Text, View } from "react-native";
+import { supabase } from "./utils/supabase";
 
-export default function EntryPoint() {
+export default function App() {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		const getTodos = async () => {
+			try {
+				const { data: todos, error } = await supabase.from("todos").select();
+
+				if (error) {
+					console.error("Error fetching todos:", error.message);
+					return;
+				}
+
+				if (todos && todos.length > 0) {
+					setTodos(todos);
+				}
+			} catch (error) {
+				console.error("Error fetching todos:", error.message);
+			}
+		};
+
+		getTodos();
+	}, []);
+
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.tsx to start your app!</Text>
-			<StatusBar style="auto" />
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<Text>Todo List</Text>
+			<FlatList
+				data={todos}
+				keyExtractor={(item) => item.id.toString()}
+				renderItem={({ item }) => <Text key={item.id}>{item.title}</Text>}
+			/>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
