6b2627b36c1e8216022f59e235af4a075a6aec9a
ryangst <ryangst.hire@gmail.com>
2024-08-30 19:01:58 -0300

    install correct dependencies

--- src/screens/auth/login.tsx
+++ src/screens/auth/login.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { AnimatedText } from "react-native-reanimated/lib/typescript/reanimated2/component/Text";
 import { version } from "../../../package.json";
 import { styles } from "./login-styles";
 
 export const Login = () => {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>{version}</Text>
+			<AnimatedText style={styles.title}>{version}</AnimatedText>
 		</View>
 	);
 };
