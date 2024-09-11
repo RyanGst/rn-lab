8512849e8d39c84bbc7c0881374942e8db941a5e
ryangst <ryangst.hire@gmail.com>
2024-09-11 11:35:31 -0300

    fix github auth

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,11 +1,17 @@
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { Platform } from "react-native";
 
 console.assert(
 	process.env.EXPO_PUBLIC_SUPABASE_URL,
 	"Missing EXPO_PUBLIC_SUPABASE_URL",
 );
+
+const supabaseUrl =
+	Platform.OS === "android"
+		? process.env.EXPO_PUBLIC_SUPABASE_URL?.replace("localhost", "10.0.2.2")
+		: process.env.EXPO_PUBLIC_SUPABASE_URL;
 
 console.assert(
 	process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
@@ -13,7 +19,7 @@ console.assert(
 );
 
 export const supabase = createClient(
-	process.env.EXPO_PUBLIC_SUPABASE_URL || "",
+	supabaseUrl || "",
 	process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
 	{
 		auth: {
