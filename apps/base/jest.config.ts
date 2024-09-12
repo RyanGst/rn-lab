586ebee7d5a5381e89851cb3286a17f01f46cbc8
ryangst <ryangst.hire@gmail.com>
2024-09-12 20:18:13 -0300

    fix unit tests

--- apps/base/jest.config.ts
+++ apps/base/jest.config.ts
@@ -4,9 +4,15 @@ const config: Config = {
 	verbose: true,
 	preset: "jest-expo",
 	transformIgnorePatterns: [
-		"node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)",
+		"<rootDir>/node_modules/(?!((jest-)?@?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)",
 	],
 	setupFiles: ["dotenv/config", "./setupJest.ts"],
+	transform: {
+		"^.+\\.(js|jsx|ts|tsx)$": [
+			"babel-jest",
+			{ configFile: "./babel.config.js" }, // <- cannot use rootDir here
+		],
+	},
 };
 
 require("dotenv").config();
